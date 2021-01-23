51177ab8bbe249190ceb11dfa371c80a90f3f5b7
shiv-kul <kulshrestha.1@iitj.ac.in>
2021-01-23 19:17:56 +0530

    new home page

--- server.js
+++ server.js
@@ -3,20 +3,30 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')   //renamed function v4 to uuidv4
-
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs')   //to render
 app.use(express.static('public'))   //express app will check all files relative to public
 app.use(express.static('views'))
-
+app.use(express.static('web'))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   //console.log("here!")
-  res.render(`home`)
+  res.render(`index`)
   //console.log(uuidV4())
 })
+
+app.post('/room', (req, res) => {
+  const id = req.body.meetid
+  res.redirect(`/${id}`)
+    //res.redirect(`/${uuidV4()}`)
+})
+
 app.get('/room', (req, res) => {
+  //console.log("shivansh")
   //res.send(uuidV4());
+  //console.log(req.body.meetid)
   res.redirect(`/${uuidV4()}`)
 })
 
